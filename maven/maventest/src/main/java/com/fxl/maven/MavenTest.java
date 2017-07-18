72c0168abb971ca7d50a12c420fc5958fd398fdf
fxl2015 <wy201505131238@163.com>
2017-07-18 14:56:23 +0800

    201707181456

---
+++ maven/maventest/src/main/java/com/fxl/maven/MavenTest.java
@@ -0,0 +1,14 @@
+package com.fxl.maven;
+
+import org.apache.log4j.Logger;
+
+public class MavenTest {
+
+	private static Logger logger = Logger.getLogger(MavenTest.class);
+
+	public static void main(String[] args) {
+		System.out.println("MavenTest.main()");
+		logger.info("logger info ...");
+	}
+
+}
